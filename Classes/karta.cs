5e85a137d1fb8dd9e2822fd7f35895d5638bc493
MisiekMMM <suszibaka2137@gmail.com>
2025-04-11 17:52:50 +0200

    Talia kart

---
+++ Classes/karta.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Pasjans;
+
+public class Karta
+{
+    public string nazwa;
+
+    public int numer; // walet J 11, królowa Q 12, król K 13
+    // true czerwony false czarny  
+    public bool kolor;
+
+    public Karta(int numer, bool kolor, string kolorSlowny) //kolorSlowny to Pik, Karo, Trefl, Kier
+    {
+        if (numer == 11)
+        {
+            nazwa = $"J {kolorSlowny}";
+        }
+        else if (numer == 12)
+        {
+            nazwa = $"Q {kolorSlowny}";
+        }
+        else if (numer == 13)
+        {
+            nazwa = $"K {kolorSlowny}";
+        }
+        else if (numer == 1)
+        {
+            nazwa = $"As {kolorSlowny}";
+        }
+        else
+        {
+            nazwa = $"{numer.ToString()} {kolorSlowny}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pasjans;
+
+public class Program
+{
+    public static void Main()// wzór tworzenia karty: Karta(int numer, bool kolor, string kolorSlowny) 
+    {
+        List<Karta> talia = new List<Karta>();
+        //Tworzenie talii kart
+        for (int i = 1; i < 14; i++)
+        {
+            talia.Add(new Karta(i, true, "Karo"));
+
+        }
+        for (int i = 1; i < 14; i++)
+        {
+            talia.Add(new Karta(i, false, "Pik"));
+
+        }
+        for (int i = 1; i < 14; i++)
+        {
+            talia.Add(new Karta(i, true, "Kier"));
+
+        }
+        for (int i = 1; i < 14; i++)
+        {
+            talia.Add(new Karta(i, false, "Trefl"));
+
+        }
+
+
+    }
+}
